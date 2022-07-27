0aa93b7ced1ce3e0b8cf252331063a5d7d78b52e
aarati0307 <aarati.singh@patancollege.edu.np>
2022-07-27 20:41:10 +0545

    week7

---
+++ week7.py
@@ -0,0 +1,27 @@
+def UniquePathHelper(i, j, r, c, A):
+
+    # boundary condition or constraints
+    if(i == r or j == c):
+        return 0
+
+    if(A[i][j] == 1):
+        return 0
+
+    # base case
+    if(i == r-1 and j == c-1):
+        return 1
+
+    return UniquePathHelper(i+1, j, r, c, A) + UniquePathHelper(i, j+1, r, c, A)
+
+
+def uniquePathsWithTiger(A):
+    r, c = len(A), len(A[0])
+
+    return UniquePathHelper(0, 0, r, c, A)
+
+
+A = [[0, 0, 0],
+     [0, 1, 0],
+     [0, 0, 0]]
+
+print(uniquePathsWithTiger(A))
